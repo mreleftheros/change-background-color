0b5241ea50dd126c22a0a9b6730a9006028e38dd
eleftheros <eleftheros_all@protonmail.com>
2021-08-11 09:05:33 +0300

    Refactor changeBackground function

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,15 @@
 const backgroundBtn = document.getElementById("backgroundBtn");
 
-const changeBackground = color => {
-  document.body.backgroundColor = color;
+const getRandomColor = () => {
+
 }
+
+// function that takes a color and changes html body background
+const changeBackground = () => {
+  const randomColor = getRandomColor();
+
+  document.body.backgroundColor = randomColor;
+};
 
 // events
 backgroundBtn.addEventListener("click", changeBackground);
