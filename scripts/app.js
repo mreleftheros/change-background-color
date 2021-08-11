59e0986e521b9815b25738bd5b6036993b1291f4
eleftheros <eleftheros_all@protonmail.com>
2021-08-11 09:20:55 +0300

    Add recursion to getRandomColor function

--- scripts/app.js
+++ scripts/app.js
@@ -5,13 +5,21 @@ const getRandomColor = () => {
   const colors = ["red", "green", "blue", "yellow", "magenta", "teal"];
   let len = colors.length;
   let randomIndex = Math.floor(Math.random() * len);
+  let pickedColor = colors[randomIndex];
+  let currectBackgroundColor = document.body.style.backgroundColor;
 
-  return colors[randomIndex];
+  if (currectBackgroundColor === pickedColor) {
+    return getRandomColor();
+  } else {
+    return pickedColor;
+  }
+
 }
 
 // function that takes a color and changes html body background
 const changeBackground = () => {
   const randomColor = getRandomColor();
+  console.log(randomColor)
 
   document.body.style.backgroundColor = randomColor;
 };
